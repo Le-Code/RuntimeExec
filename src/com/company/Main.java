0b28b5692773851e8c3e4eedf99cca65f5356387
Le-Code <1054145364@qq.com>
2020-09-10 00:13:17 +0800

    add a SubEventQueue

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,26 @@
 package com.company;
 
+import sun.misc.Unsafe;
+
 import java.io.IOException;
+import java.lang.reflect.Field;
 
 public class Main {
 
+    private static Unsafe UNSAFE;
+
+    static {
+        try {
+            Field field = Unsafe.class.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            UNSAFE = (Unsafe) field.get(null);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) throws IOException {
 	// write your code here
+        Unsafe unsafe = Unsafe.getUnsafe();
     }
 }
